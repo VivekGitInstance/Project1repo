70e447093c3bf596680a5fac225f6f6e5e25dae2
devopsvivek <devopvivek@gmail.com>
2021-05-29 18:12:15 +0000

    added test2 file along with some changes

--- test1.java
+++ test1.java
@@ -2,6 +2,7 @@ class test1
 {
 public static void main (String args[])
 {
-	system.out println("My first work");
+	System.out.println("My first work");
+	System.out.println("My secong addition of code");
 } 
 }
